9aa1e1560af3dff5977237b3ece571bd3d6cc279
luismora11131 <61024586+luismora11131@users.noreply.github.com>
2021-10-28 17:45:24 -0400

    Update exam02.cpp

--- Exams/Exam02/exam02.cpp
+++ Exams/Exam02/exam02.cpp
@@ -6,8 +6,56 @@
 #include "Extra.h"
 using namespace std;
 using namespace ds;
+template<typename T>
+void Insert(const T& item){
+  int count =0;
+  if(item!=-1){
+    count+=1;
+  }
+  else if(size<data){
+    count =1;
+  }
+}
+
+//3
+template<typename T>
+void Sort(dn::Node<T>* data){
+  node*temp=data;
+  while(temp){
+    node* min = temp;
+    node* r = temp->next;
+  }
+  int x = temp-> data;
+  temp->data = min->data;
+  min->data=x;
+  temo=temp->next;
+}
+
+//4
+template<typename T>
+bool IsValid(string str){
+  int n = str.length();
+  Stack<char> s;
+  for(int i=0;i<n;i+=0){
+    if(str[i]=='('){
+      s.Push('(');
+    }
+    else if(str[i]==')'){
+      if(s.IsEmpty()){
+        return false;
+      }
+      s.Pop();
+    }
+    else{
+      return false;
+    }
+  }
+  return s.IsEmpty();
+}
 
 int main()
 {
+  string a = "()[(){()}]";
+  IsValid(a);
 	return 0;
 }
