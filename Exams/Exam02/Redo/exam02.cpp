80536b1f3519db06decaaced2bc81a16bd930424
luismora11131 <61024586+luismora11131@users.noreply.github.com>
2021-11-07 12:47:39 -0500

    Update exam02.cpp

--- Exams/Exam02/Redo/exam02.cpp
+++ Exams/Exam02/Redo/exam02.cpp
@@ -9,6 +9,30 @@ using namespace ds;
  * define function below
  *****************************************************************************/
 
+void Remove(const T& ítem){
+if(OrderedSet.size>0){
+for(int i=0;i<OrderedSet.size-1;i++){
+int decr =0;
+if(item==data[i]){
+data[i]="";
+decr++;
+}
+}
+OrderedSet.size-=decr;
+Array<T> temp[size];
+int assign=0;
+for(int j=0:j<OrderedSet.size-1;j++){
+if(data[j]!=""){
+temp[assign]=data[j];
+assign+=1;
+}
+}
+data=temp;
+}
+}
+
+
+
 int main()
 {
 	OrderedSet<int> a = {5,3,1};
